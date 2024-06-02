807f7b0fb9a4a9c596b6c71ea4ce37152dbee04e
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-03 01:26:00 +0200

    updated colors

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <section class="bg-red-800 text-white p-8 text-center">
-    <h1 class="text-4xl mb-4">Für jeden Anlass dein Handmade Geschenk</h1>
-    <p class="mb-4">Bei DotForYou ist alles selbstgemacht...</p>
+  <section class="bg-primary text-white p-8 text-center">
+    <h1 class="text-4xl text-text_on_primary mb-4">Für jeden Anlass dein Handmade Geschenk</h1>
+    <p class="mb-4 text-text_on_primary">Bei DotForYou ist alles selbstgemacht...</p>
     <div class="space-x-4">
       <button class="bg-green-500 px-4 py-2">Zum Shop</button>
       <button class="bg-gray-500 px-4 py-2">Kontakt</button>
