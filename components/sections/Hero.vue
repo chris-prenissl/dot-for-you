2033a6b65ebdedfe80253372b583bf2d3b3186d0
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-08 18:02:21 +0200

    created professional hero page that adapts to screen sizes with background and blur

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,10 +1,20 @@
 <template>
-  <section class="bg-primary text-white p-8 text-center">
-    <h1 class="text-4xl text-text_on_primary mb-4">Für jeden Anlass dein Handmade Geschenk</h1>
-    <p class="mb-4 text-text_on_primary">Bei DotForYou ist alles selbstgemacht...</p>
-    <div class="space-x-4">
-      <button class="bg-green-500 px-4 py-2">Zum Shop</button>
-      <button class="bg-gray-500 px-4 py-2">Kontakt</button>
+  <section
+      class="relative flex flex-col items-center md:items-start justify-center h-dvh bg-cover bg-center"
+      style="background-image: url('/gallery_images/gallery_3.jpg')">
+    <div class="block px-4 md:px-32 box-border">
+      <div class="block text-left max-w-full p-8 w-96 backdrop-blur-sm rounded-xl">
+        <h1 class="text-3xl mb-4">Mit Liebe handgemacht</h1>
+        <p class="text-lg mb-4">
+          Aus einer Vielzahl an Materialien gestalte ich individuelle Dekorationen und Geschenke für dich.
+          In meinem Sortiment findest du einzelne Artikel, Komplettsets sowie personalisierte Designs nach deinen
+          Wünschen.
+        </p>
+        <div class="flex flex-col md:flex-row items-center md:items-start md:space-x-4 md:space-y-0 space-x-0 space-y-4">
+          <a href="/" class="bg-primary p-4 border-gray-200 border-2 rounded-xl text-center w-1/2 md:w-max">Zum Shop</a>
+          <a href="/" class="bg-teal-400 p-4 border-gray-200 border-2 rounded-xl text-center w-1/2 md:w-max">Kontakt</a>
+        </div>
+      </div>
     </div>
   </section>
 </template>
