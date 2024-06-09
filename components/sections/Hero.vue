89ff7d7626dda19ec384953b9fbb87395617db50
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-09 11:05:33 +0200

    updated layout spacing and added base color

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -1,18 +1,18 @@
 <template>
   <section
-      class="relative flex flex-col items-center md:items-start justify-center h-vh bg-cover bg-center"
+      class="relative flex flex-col items-center md:items-start lg:items-center justify-center h-vh bg-cover bg-center"
       style="background-image: url('/gallery_images/gallery_3.jpg')">
-    <div class="block px-4 md:px-32 box-border">
-      <div class="block text-left max-w-full p-8 w-96 backdrop-blur-sm rounded-xl">
-        <h1 class="text-3xl mb-4">Mit Liebe handgemacht</h1>
-        <p class="text-lg mb-4">
+    <div class="px-4 max-w-screen-lg box-border">
+      <div class="max-w-full min-w-80 p-8 m-4 backdrop-blur-sm rounded-xl">
+        <h1 class="lg:text-6xl md:text-4xl text-5xl mb-4">Mit Liebe handgemacht</h1>
+        <p class="text-left lg:text-3xl md:text-2xl text-2xl mb-4">
           Aus einer Vielzahl an Materialien gestalte ich individuelle Dekorationen und Geschenke für dich.
           In meinem Sortiment findest du einzelne Artikel, Komplettsets sowie personalisierte Designs nach deinen
           Wünschen.
         </p>
-        <div class="flex flex-col md:flex-row items-center md:items-start md:space-x-4 md:space-y-0 space-x-0 space-y-4">
-          <a href="/" class="bg-primary p-4 border-gray-200 border-2 rounded-xl text-center w-1/2 md:w-max">Zum Shop</a>
-          <a href="/" class="bg-teal-400 p-4 border-gray-200 border-2 rounded-xl text-center w-1/2 md:w-max">Kontakt</a>
+        <div class="flex flex-wrap justify-center sm:justify-start gap-8">
+          <a href="/" class="bg-primary p-6 border-pink-400 border-2 rounded-xl text-xl text-center">Zum Shop</a>
+          <a href="/" class="bg-base p-6 border-pink-400 border-2 rounded-xl text-xl text-center">Kontakt</a>
         </div>
       </div>
     </div>
