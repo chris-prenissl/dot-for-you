7023e3ff6ca99febbe7391570279ada124e8b171
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-20 16:42:07 +0200

    extracted image component from Gallery and CreatorStory

---
+++ components/elements/ArtImage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{
+  srcPath: string,
+  title: string,
+}>();
+</script>
+
+<template>
+  <img class="max-h-60lvh max-w-60lvw object-cover content-center" :src="srcPath" :alt="title" />
+</template>
