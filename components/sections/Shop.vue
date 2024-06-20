7796c9fedcf75dbc5455e2845c44099d1f48601d
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-20 16:46:18 +0200

    updated padding

--- components/sections/Shop.vue
+++ components/sections/Shop.vue
@@ -3,8 +3,8 @@ import ButtonAnchorPrimary from "~/components/elements/ButtonAnchorPrimary.vue";
 </script>
 
 <template>
-  <section id="shop" class="h-lvh flex flex-col items-center justify-center gap-16 border-y-2">
-      <h1 class="text-4xl">Shop</h1>
+  <section id="shop" class="min-h-lvh flex flex-col items-center justify-center gap-16 p-20">
+      <h1 class="text-4xl font-bold">Shop</h1>
       <img class="object-cover content-center" src="/public/gallery_images/gallery_1.jpg" alt="Shop">
       <ul>
         <li>1. Bestellen</li>
