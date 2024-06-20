0be659162381be3c14a8891150a15964aaa47c62
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-20 16:44:38 +0200

    better sizing of images with flex in mobile

--- components/elements/GallerySlideItem.vue
+++ components/elements/GallerySlideItem.vue
@@ -1,17 +1,25 @@
 <script setup lang="ts">
-defineProps({
-  title: String,
-  imgUrl: String,
-  description: String,
-});
+import ArtImage from "~/components/elements/ArtImage.vue";
+
+defineProps<{
+  title: string,
+  imgUrl: string,
+  description: string,
+}>();
 </script>
 
 <template>
-  <div class="grid grid-rows-2 md:grid-cols-2 md:grid-rows-1 justify-items-center gap-4 p-16">
-    <img class="object-cover content-center" :src="imgUrl" :alt="title"/>
-    <div class="flex flex-col gap-4">
+  <div class="flex flex-col justify-center md:grid md:grid-cols-2 md:grid-rows-1 justify-items-center gap-4 p-20">
+    <ArtImage :src-path="imgUrl" :title="title" class="h-40lvh md:h-full shadow-lg"/>
+    <div class="flex flex-col gap-4 md:justify-center p-16 shadow-lg bg-base backdrop-blur-2xl">
       <h1 class="text-4xl">{{title}}</h1>
-      <p>{{description}}</p>
+      <p class="overflow-hidden">{{description}}</p>
     </div>
   </div>
 </template>
+
+<style scoped>
+.h-40lvh {
+  height: 50lvh;
+}
+</style>
