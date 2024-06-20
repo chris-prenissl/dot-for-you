41652c0984139421ad2ecb14c0d91c3635f0a941
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-20 16:41:25 +0200

    added linear gradiant to About

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,6 +1,12 @@
 <template>
-  <section id="about" class="h-lvh flex flex-col items-center py-40 gap-16 border-y-2">
-      <h2 class="text-3xl">About</h2>
+  <section id="about" class="about-linear-gradient min-h-lvh flex flex-col items-center py-40 gap-16">
+      <h2 class="text-3xl font-bold">About</h2>
       <p>Melanie ist eine Künstlerin ...</p>
   </section>
 </template>
+
+<style>
+.about-linear-gradient {
+  background: linear-gradient(to top, #f7d3df 10%, #f9f9f9 100%);
+}
+</style>
