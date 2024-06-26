4b03b5bb0db83f46ad692ffd99b080ac28e75c22
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-26 16:20:42 +0200

    more distance to bullets in gallery on mobile

--- components/elements/GallerySlideItem.vue
+++ components/elements/GallerySlideItem.vue
@@ -9,7 +9,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="m-2 md:p-8 flex flex-col justify-center md:grid md:grid-cols-2 md:grid-rows-1 justify-items-center gap-4">
+  <div class="mx-2 mb-8 md:m-2 md:p-8 flex flex-col justify-center md:grid md:grid-cols-2 md:grid-rows-1 justify-items-center gap-4">
     <ArtImage class="h-50svh w-full shadow-lg" :src-path="imgUrl" :title="title"/>
     <div class="p-8 flex flex-col gap-2 md:justify-center shadow-lg bg-base backdrop-blur-2xl">
       <h1 class="text-4xl">{{title}}</h1>
