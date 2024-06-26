35a75a67990fe5b9a306b65907455c065fd7c80d
Christoh Prenissl <christophprenissl@gmail.com>
2024-06-26 16:47:39 +0200

    added first iteration of about section with image

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,7 +1,16 @@
 <template>
-  <section id="about" class="about-linear-gradient min-h-lvh flex flex-col items-center py-40 gap-16">
-      <h2 class="text-3xl font-bold">About</h2>
-      <p>Melanie ist eine Künstlerin ...</p>
+  <section id="about" class="min-w-20 p-20 about-linear-gradient min-h-lvh flex flex-col md:flex-row items-center gap-16">
+    <div class="md:max-w-screen-lg">
+      <h2 class="mb-4 md:mb-16 text-3xl md:text-6xl font-bold">Melanie</h2>
+      <p class="text-xl md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.
+        Cras venenatis euismod malesuada. Nulla facilisi. Curabitur id dui ac lacus dictum auctor. Nam eget dui vitae
+        justo aliquet ullamcorper. Aenean tincidunt mauris vel turpis interdum, vel varius ante placerat. Praesent
+        id metus ut leo elementum semper. Sed at dictum nunc, vel malesuada velit. Nullam sit amet orci ut purus
+        fermentum elementum. Proin condimentum, orci eu dapibus ullamcorper, enim lectus accumsan eros, sit amet
+        tempor sapien nisl ac libero. Integer euismod est at turpis faucibus.
+      </p>
+    </div>
+    <img class="h-full w-full md:max-w-screen-lg content-center object-contain -order-1 md:order-1" src="/public/melanie/melanie.png" alt="Melanie" />
   </section>
 </template>
 
