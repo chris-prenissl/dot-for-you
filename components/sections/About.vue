d3492eb16fb9c7cf07d14460fbb85d44bd71eecd
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-06 19:43:48 +0200

    Update text and layout of About

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,16 +1,15 @@
 <template>
-  <section id="about" class="min-w-20 p-20 about-linear-gradient min-h-lvh flex flex-col md:flex-row items-center gap-16">
-    <div class="md:max-w-screen-lg">
-      <h2 class="mb-4 md:mb-16 text-3xl md:text-6xl font-bold">Melanie</h2>
-      <p class="text-xl md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.
-        Cras venenatis euismod malesuada. Nulla facilisi. Curabitur id dui ac lacus dictum auctor. Nam eget dui vitae
-        justo aliquet ullamcorper. Aenean tincidunt mauris vel turpis interdum, vel varius ante placerat. Praesent
-        id metus ut leo elementum semper. Sed at dictum nunc, vel malesuada velit. Nullam sit amet orci ut purus
-        fermentum elementum. Proin condimentum, orci eu dapibus ullamcorper, enim lectus accumsan eros, sit amet
-        tempor sapien nisl ac libero. Integer euismod est at turpis faucibus.
+  <section id="about" class="p-20 about-linear-gradient min-h-lvh flex flex-wrap items-center justify-center gap-8">
+    <img class="w-full max-h-dvh md:w-50svh md:max-w-screen-lg content-center object-contain" src="/public/melanie/melanie.png" alt="Melanie" />
+    <div class="min-w-60 md:max-w-screen-md">
+      <h2 class="mb-4 md:mb-16 text-2xl md:text-4xl font-bold">Melanie</h2>
+      <p class="text-lg md:text-2xl">Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
+        drei Katzen in Burglengenfeld. Hauptberuflich arbeite ich als allgemeinbildende Lehrerin an einer
+        sonderpädagoischen Berufsschule. Meinen Ausgleich finde ich in meinem kreativen Hobby, welches sich zu einem
+        „Kleingewerbe“ entwickelt hat. Dafür bedanke ich mich von Herzen, denn ohne das große Interesse von euch,
+        wäre das nie möglich gewesen.
       </p>
     </div>
-    <img class="h-full w-full md:max-w-screen-lg content-center object-contain -order-1 md:order-1" src="/public/melanie/melanie.png" alt="Melanie" />
   </section>
 </template>
 
