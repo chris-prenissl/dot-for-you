9782deedadf4f1ee9617d436346bd41ca3dd1bb3
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-24 13:23:54 +0200

    Update Contact for correct links

--- components/sections/Contact.vue
+++ components/sections/Contact.vue
@@ -1,24 +1,24 @@
 <template>
   <section id="contact" class="m-auto min-h-50svh flex flex-col justify-between align-middle bg-primary text-base p-20">
     <h1 class="text-4xl text-center font-bold">Kontakt</h1>
-    <div class="text-center flex flex-col items-center justify-center gap-4 h-full">
+    <div class="text-center flex flex-col items-center justify-center gap-16 h-full">
       <h3 class="text-2xl mb-4">Folge mir für weitere Ideen</h3>
-      <div class="flex justify-center space-x-4 mb-4">
-        <a href="#" class="flex flex-row justify-center items-center gap-2">
-          <svg class="h-12" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+      <div class="flex justify-center space-x-8 sd:space-x-16 mb-4">
+        <a href="https://wa.me/491637917232" target="_blank" class="flex flex-row justify-center items-center gap-2">
+          <svg class="h-10" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path class="fill-text_on_primary" d="M12.228,5.84a5.961,5.961,0,0,0-5.052,9.13l.142.225-.6,2.2L8.971,16.8l.218.129a5.947,5.947,0,0,0,3.034.83h0a5.961,5.961,0,0,0,0-11.922Zm3.506,8.52a1.841,1.841,0,0,1-1.21.852,2.447,2.447,0,0,1-1.128-.071,10.013,10.013,0,0,1-1.022-.377,7.991,7.991,0,0,1-3.061-2.7,3.48,3.48,0,0,1-.732-1.853,2.012,2.012,0,0,1,.627-1.495.662.662,0,0,1,.478-.224c.12,0,.239,0,.344.006s.257-.041.4.308.508,1.24.552,1.33a.329.329,0,0,1,.015.314,1.186,1.186,0,0,1-.179.3c-.089.1-.188.233-.269.314s-.183.186-.078.365a5.363,5.363,0,0,0,1,1.241,4.857,4.857,0,0,0,1.441.889c.179.09.283.075.388-.044s.448-.523.567-.7.239-.149.4-.089,1.045.493,1.224.582.3.135.344.21A1.507,1.507,0,0,1,15.734,14.36ZM21,2H3A1,1,0,0,0,2,3V21a1,1,0,0,0,1,1H21a1,1,0,0,0,1-1V3A1,1,0,0,0,21,2ZM12.225,18.973h0A7.165,7.165,0,0,1,8.8,18.1l-3.8,1,1.018-3.716a7.172,7.172,0,1,1,6.214,3.591Z"/>
           </svg>
           WhatsApp
         </a>
-        <a href="#" class="flex flex-row justify-center items-center gap-2">
-          <svg class="h-12" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <a href="https://www.instagram.com/dot_for_you_" target="_blank" class="flex flex-row justify-center items-center gap-2">
+          <svg class="h-10" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path class="fill-text_on_primary" fill-rule="evenodd" clip-rule="evenodd" d="M12 18C15.3137 18 18 15.3137 18 12C18 8.68629 15.3137 6 12 6C8.68629 6 6 8.68629 6 12C6 15.3137 8.68629 18 12 18ZM12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" fill="#0F0F0F"/>
             <path class="fill-text_on_primary" d="M18 5C17.4477 5 17 5.44772 17 6C17 6.55228 17.4477 7 18 7C18.5523 7 19 6.55228 19 6C19 5.44772 18.5523 5 18 5Z" fill="#0F0F0F"/>
             <path class="fill-text_on_primary" fill-rule="evenodd" clip-rule="evenodd" d="M1.65396 4.27606C1 5.55953 1 7.23969 1 10.6V13.4C1 16.7603 1 18.4405 1.65396 19.7239C2.2292 20.8529 3.14708 21.7708 4.27606 22.346C5.55953 23 7.23969 23 10.6 23H13.4C16.7603 23 18.4405 23 19.7239 22.346C20.8529 21.7708 21.7708 20.8529 22.346 19.7239C23 18.4405 23 16.7603 23 13.4V10.6C23 7.23969 23 5.55953 22.346 4.27606C21.7708 3.14708 20.8529 2.2292 19.7239 1.65396C18.4405 1 16.7603 1 13.4 1H10.6C7.23969 1 5.55953 1 4.27606 1.65396C3.14708 2.2292 2.2292 3.14708 1.65396 4.27606ZM13.4 3H10.6C8.88684 3 7.72225 3.00156 6.82208 3.0751C5.94524 3.14674 5.49684 3.27659 5.18404 3.43597C4.43139 3.81947 3.81947 4.43139 3.43597 5.18404C3.27659 5.49684 3.14674 5.94524 3.0751 6.82208C3.00156 7.72225 3 8.88684 3 10.6V13.4C3 15.1132 3.00156 16.2777 3.0751 17.1779C3.14674 18.0548 3.27659 18.5032 3.43597 18.816C3.81947 19.5686 4.43139 20.1805 5.18404 20.564C5.49684 20.7234 5.94524 20.8533 6.82208 20.9249C7.72225 20.9984 8.88684 21 10.6 21H13.4C15.1132 21 16.2777 20.9984 17.1779 20.9249C18.0548 20.8533 18.5032 20.7234 18.816 20.564C19.5686 20.1805 20.1805 19.5686 20.564 18.816C20.7234 18.5032 20.8533 18.0548 20.9249 17.1779C20.9984 16.2777 21 15.1132 21 13.4V10.6C21 8.88684 20.9984 7.72225 20.9249 6.82208C20.8533 5.94524 20.7234 5.49684 20.564 5.18404C20.1805 4.43139 19.5686 3.81947 18.816 3.43597C18.5032 3.27659 18.0548 3.14674 17.1779 3.0751C16.2777 3.00156 15.1132 3 13.4 3Z" fill="#0F0F0F"/>
           </svg>
           Instagram
         </a>
-        <a href="#" class="flex flex-row justify-center items-center gap-2">
+        <a href="https://m.facebook.com/61560145346862/" target="_blank" class="flex flex-row justify-center items-center gap-2">
           <svg class="h-12" viewBox="-5.5 0 32 32" xmlns="http://www.w3.org/2000/svg">
             <path class="fill-text_on_primary" d="M1.188 5.594h18.438c0.625 0 1.188 0.563 1.188 1.188v18.438c0 0.625-0.563 1.188-1.188 1.188h-18.438c-0.625 0-1.188-0.563-1.188-1.188v-18.438c0-0.625 0.563-1.188 1.188-1.188zM14.781 17.281h2.875l0.125-2.75h-3v-2.031c0-0.781 0.156-1.219 1.156-1.219h1.75l0.063-2.563s-0.781-0.125-1.906-0.125c-2.75 0-3.969 1.719-3.969 3.563v2.375h-2.031v2.75h2.031v7.625h2.906v-7.625z"></path>
           </svg>
@@ -26,18 +26,16 @@
         </a>
       </div>
       <div class="flex justify-center space-x-4 mb-4">
-        <a href="tel:+49193229919" class="flex items-center space-x-2">
-          <i class="fas fa-phone"></i><span>+49 1932 29919</span>
+        <a href="tel:+491637917232" target="_blank" class="flex items-center space-x-2">
+          <i class="fas fa-phone"></i><span>+49 163 7917232</span>
         </a>
-        <a href="https://goo.gl/maps/..." class="flex items-center space-x-2">
+        <a href="https://maps.apple.com/?address=Augustenstra%C3%9Fe%2020,%2093133%20Burglengenfeld,%20Deutschland&ll=49.205873,12.053873" target="_blank" class="flex items-center space-x-2">
           <i class="fas fa-map-marker-alt"></i><span>93133 Burglengenfeld, Augustenstraße 20</span>
         </a>
-        <a href="mailto:info@dotforyou.de" class="flex items-center space-x-2">
-          <i class="fas fa-envelope"></i><span>info@dotforyou.de</span>
+        <a href="mailto:dotforyou@gmx.de" target="_blank" class="flex items-center space-x-2">
+          <i class="fas fa-envelope"></i><span>dotforyou@gmx.de</span>
         </a>
       </div>
     </div>
   </section>
 </template>
-<script setup lang="ts">
-</script>
