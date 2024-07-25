96f732e2fa3fa9da2cce9644aae26b6238c98ca5
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-25 12:37:46 +0200

    Change layouting to flex

--- components/elements/StoryItem.vue
+++ components/elements/StoryItem.vue
@@ -10,13 +10,13 @@ defineProps<{
 </script>
 
 <template>
-  <div class="grid grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-2 drop-shadow-lg"
+  <div class="flex flex-col sm:flex-row gap-2 drop-shadow-lg"
     :data-aos="gridOrder === 0 ? 'fade-right' : 'fade-left'">
-    <ArtImage v-if="gridOrder !== 1" class="w-full md:h-full" :src-path="srcPath" :title="title" />
-    <div class="bg-base text-text_on_primary p-8">
-      <h1 class="text-4xl py-4">{{ title }}</h1>
+    <ArtImage v-if="gridOrder !== 1" class="sm:w-1/2" :src-path="srcPath" :title="title" />
+    <div class="sm:w-1/2 bg-base text-text_on_primary p-8">
+      <h1 class="text-3xl py-4">{{ title }}</h1>
       <p>{{ description }}</p>
     </div>
-    <ArtImage v-if="gridOrder === 1" class="w-full md:h-full order-first md:order-1" :src-path="srcPath" :title="title" />
+    <ArtImage v-if="gridOrder === 1" class="sm:w-1/2 order-first md:order-1" :src-path="srcPath" :title="title" />
   </div>
 </template>
