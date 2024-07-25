2325aa0905c92ef51260da068c181b32c88e45eb
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-25 16:13:14 +0200

    Apply backdrop blur

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="about" class="about-linear-gradient backdrop-blur flex flex-wrap items-center justify-center gap-8">
     <img data-aos="fade-right" class="sm:w-40svh md:max-w-screen-lg border-8 content-center object-cover shadow-2xl" src="/public/melanie/melanie.jpg" alt="Melanie" />
-    <div data-aos="fade-left" class="sm:w-40svh md:max-w-screen-lg p-4 md:p-16 bg-primary shadow-2xl">
+    <div data-aos="fade-left" class="sm:w-40svh md:max-w-screen-lg p-4 md:p-16 rounded-l backdrop-blur-2xl">
       <h2 class="mb-4 md:mb-16 text-3xl font-bold">Melanie</h2>
-      <p class="md:text-lg md:max-w-screen-lg">Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
+      <p class="md:text-lg md:max-w-screen-lg text-text_on_primary p-2 rounded-l">Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
         drei Katzen in Burglengenfeld. Hauptberuflich arbeite ich als allgemeinbildende Lehrerin an einer
         sonderpädagoischen Berufsschule. Meinen Ausgleich finde ich in meinem kreativen Hobby, welches sich zu einem
         „Kleingewerbe“ entwickelt hat. Dafür bedanke ich mich von Herzen, denn ohne das große Interesse von euch,
