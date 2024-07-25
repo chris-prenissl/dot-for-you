bd35ad6153be3fd407d1e352bf1424e7d64920ff
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-26 01:35:30 +0200

    Better gradient

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,9 +1,9 @@
 <template>
   <section id="about" class="about-linear-gradient backdrop-blur flex flex-wrap items-center justify-center gap-8">
     <img data-aos="fade-right" class="sm:w-40svh md:max-w-screen-lg border-8 content-center object-cover shadow-2xl" src="/public/melanie/melanie.jpg" alt="Melanie" />
-    <div data-aos="fade-left" class="sm:w-40svh md:max-w-screen-lg p-4 md:p-16 rounded-l backdrop-blur-2xl">
-      <h2 class="mb-4 md:mb-16 text-3xl font-bold">Melanie</h2>
-      <p class="md:text-lg md:max-w-screen-lg text-text_on_primary p-2 rounded-l">Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
+    <div data-aos="fade-left" class="sm:w-40svh md:max-w-screen-lg p-2 sm:p-4 md:p-16 rounded-l backdrop-blur-2xl shadow-xl">
+      <h2 class="p-2 sm:p4 text-4xl font-bold">Melanie</h2>
+      <p class="p-2 sm:p4 md:text-lg md:max-w-screen-l text-justify text-text_on_primary border-dotted">Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
         drei Katzen in Burglengenfeld. Hauptberuflich arbeite ich als allgemeinbildende Lehrerin an einer
         sonderpädagoischen Berufsschule. Meinen Ausgleich finde ich in meinem kreativen Hobby, welches sich zu einem
         „Kleingewerbe“ entwickelt hat. Dafür bedanke ich mich von Herzen, denn ohne das große Interesse von euch,
@@ -16,7 +16,7 @@
 <style scoped>
 .about-linear-gradient {
   background-size: cover !important;
-  backdrop-filter: blur(30px);
-  background: linear-gradient(to top, rgba(247, 211, 223, 0.8) 10%, rgba(249, 249, 249, 0.8) 100%), url("/public/melanie/melanie_bg.png") no-repeat center;
+  backdrop-filter: blur(10px);
+  background: linear-gradient(to bottom, rgba(247, 211, 223, 0.85) 0%, rgba(249, 249, 249, 0.94) 100%), url("/public/melanie/melanie_bg.png") no-repeat center;
 }
 </style>
