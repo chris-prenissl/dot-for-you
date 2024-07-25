6fb5eb1697c844d3a1920228e97815224fe20920
Christoh Prenissl <christophprenissl@gmail.com>
2024-07-26 01:36:59 +0200

    Create components for Contact

---
+++ components/elements/AddressItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  href: string,
+  text: string,
+}>();
+</script>
+
+<template>
+  <a :href="href" target="_blank"
+     class="w-full lg:w-auto lg:h-full flex flex-col items-center gap-4 bg-text_on_primary border border-dashed p-2">
+    <slot/>
+    <p>{{ text }}</p>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
---
+++ components/elements/SocialItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  href: string,
+  text: string,
+}>()
+</script>
+
+<template>
+  <a :href="href" target="_blank"
+     class="flex flex-row justify-center items-center gap-4">
+    <slot/>
+    <p class="text-text_on_primary">{{ text }}</p>
+  </a>
+</template>
+
+<style scoped>
+
+</style>
