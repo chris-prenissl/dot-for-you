4f67515b564e6708d6efceb5baef251edd476d37
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-05 16:22:58 +0200

    Apply high z so CookieDialog is above content

--- components/elements/CookieDialog.vue
+++ components/elements/CookieDialog.vue
@@ -10,7 +10,7 @@ function onAcceptCookies(): void {
 </script>
 
 <template>
-  <div v-if="!cookieAccept" class="fixed bottom-0 w-full flex flex-wrap gap-2 justify-center items-center bg-light_white p-2">
+  <div v-if="!cookieAccept" class="fixed bottom-0 w-full z-50 flex flex-wrap gap-2 justify-center items-center bg-light_white p-2">
     <p>Wir speichern lediglich ein Cookie für die Kenntnisnahme dieses Dialogs. Weitere Cookies oder Daten werden nicht erfasst.</p>
     <button @click="onAcceptCookies" class="p-4 bg-primary border-pink-400 border rounded-sm text-center text-l hover:bg-light_white hover:text-logo_text hover:drop-shadow-lg">Zustimmung</button>
   </div>
