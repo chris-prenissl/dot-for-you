b6a888904d838377e2ffcd859beaa57a9deb206c
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-05 15:09:17 +0200

    Move logo into component and style bigger

--- components/sections/Navbar.vue
+++ components/sections/Navbar.vue
@@ -1,38 +1,40 @@
 <script setup lang="ts">
-  const menuOpen = ref(false);
-  const heroIntersection = ref(false);
+import Logo from "~/components/svgs/Logo.vue";
 
-  function closeMenu() {
-    menuOpen.value = false;
-  }
+const menuOpen = ref(false);
+const heroIntersection = ref(false);
 
-  function onToggleMenu(): void {
-    menuOpen.value = !menuOpen.value;
-  }
+function closeMenu() {
+  menuOpen.value = false;
+}
 
-  onMounted(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-          heroIntersection.value = entry.isIntersecting;
-      })
+function onToggleMenu(): void {
+  menuOpen.value = !menuOpen.value;
+}
+
+onMounted(() => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      heroIntersection.value = entry.isIntersecting;
     })
-    const logo = document.getElementById('hero-element')!;
-    observer.observe(logo);
-  });
+  })
+  const logo = document.getElementById('hero-element')!;
+  observer.observe(logo);
+});
 </script>
 
 <template>
-  <header :class="menuOpen ? 'h-auto' : 'h-24'" class="h-20 px-2 w-full nav-radial-gradiant bg-primary fixed top-0 z-50 shadow">
-    <div :class="menuOpen ? 'flex-wrap' : '' " class="max-w-screen-lg py-4 mx-auto flex sm:flex-nowrap items-center justify-between">
-      <div id="logo">
-        <a href="#" class="cursor-pointer">
-          <img class="h-12 w-auto object-contain" src="assets/logo.svg" alt=""/>
-        </a>
-      </div>
+  <header :class="menuOpen ? 'h-auto' : ''" class="h-24 w-full nav-radial-gradiant bg-primary fixed top-0 z-50 shadow">
+    <div :class="menuOpen ? 'flex-wrap' : '' "
+         class="max-w-screen-lg mx-auto flex sm:flex-nowrap items-center justify-between p-2">
+      <a id="logo" href="#" class="cursor-pointer">
+        <Logo class="h-18 sm:w-auto"/>
+      </a>
       <input @input="onToggleMenu" id="navbar-open" class="peer hidden" type="checkbox"/>
       <label class="p-2 mt-2 cursor-pointer sm:hidden" for="navbar-open">
         <span class="sr-only">Toggle Navigation</span>
-        <svg v-if="!menuOpen" xmlns="http://www.w3.org/2000/svg" width="0.88em" height="1em" preserveAspectRatio="xMidYMid meet"
+        <svg v-if="!menuOpen" xmlns="http://www.w3.org/2000/svg" width="0.88em" height="1em"
+             preserveAspectRatio="xMidYMid meet"
              viewBox="0 0 448 512">
           <path fill="currentColor"
                 d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/>
@@ -42,7 +44,9 @@
       <nav :class="!menuOpen ? 'hidden' : ''" class="w-full sm:w-auto text-nowrap p-8 sm:block">
         <ul aria-label="Primary" class="text-2xl sm:text-xs flex text-right flex-col sm:flex-row gap-2 sm:gap-8">
           <li>
-            <a @click="closeMenu" class="hover:text-logo_text" href="#handmade"><div class="w-full">Handmade</div></a>
+            <a @click="closeMenu" class="hover:text-logo_text" href="#handmade">
+              <div class="w-full">Handmade</div>
+            </a>
           </li>
           <li>
             <a @click="closeMenu" class="hover:text-logo_text" href="#gallery">Galerie</a>
@@ -57,7 +61,8 @@
       </nav>
     </div>
   </header>
-  <div @click="closeMenu" :class="!menuOpen ? 'hidden' : ''" class="z-40 md:hidden overlay fixed inset-0 bg-transparent"></div>
+  <div @click="closeMenu" :class="!menuOpen ? 'hidden' : ''"
+       class="z-40 md:hidden overlay fixed inset-0 bg-transparent"></div>
 </template>
 
 <style scoped>
