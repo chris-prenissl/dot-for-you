987ff10332562acb589d6ba7de5528de2a658ac8
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-07 17:17:11 +0200

    Make footer links open in new page

--- components/sections/Footer.vue
+++ components/sections/Footer.vue
@@ -1,13 +1,12 @@
 <template>
   <footer class="text-text_default p-4">
     <div class="flex justify-center space-x-4">
-      <a href="/impressum" class="hover:underline">Impressum</a>
-      <a href="/datenschutz" class="hover:underline">Datenschutz</a>
+      <a target="_blank" href="/impressum" class="hover:underline">Impressum</a>
+      <a target="_blank" href="/datenschutz" class="hover:underline">Datenschutz</a>
     </div>
 
     <div class="text-center mt-2 text-sm">
       &copy; 2024 - {{ new Date().getFullYear() }} Christoph Prenissl. All rights reserved.
-      Thanks to svgrepo.com for the social media icons (<a href="https://www.svgrepo.com/page/licensing/#CC%20Attribution">License</a>).
     </div>
   </footer>
 </template>
