ac7b4683d9cbfee0ea70156ed3a72f63f96eef71
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-07 16:47:21 +0200

    Update background image

--- components/sections/Hero.vue
+++ components/sections/Hero.vue
@@ -5,18 +5,18 @@ import ButtonAnchorPrimary from "~/components/elements/ButtonAnchorPrimary.vue";
 
 <template>
   <section id="hero" class="hero-background-img content-center bg-cover bg-center">
-      <div id="hero-element" class="max-w-screen-md p-8 m-auto rounded-sm bg-text_on_primary bg-opacity-75">
-        <h1 class="text-3xl font-bold text-light_white mb-8">Mit Liebe handgemacht</h1>
-        <p class="text-xl text-light_white mb-8">
-          Aus einer Vielzahl an Materialien gestalte ich individuelle Dekorationen, Geschenke und Mitbringsel.
-          In meinem Sortiment findest du einzelne Artikel, Komplettsets sowie personalisierte Designs nach deinen
-          Wünschen.
-        </p>
-        <div class="flex flex-col sm:flex-row flex-wrap gap-8 sm:gap-32">
-          <ButtonAnchorPrimary title="Zum WhatsApp Shop" path="https://wa.me/c/491637917232"/>
-          <ButtonAnchorSecondary title="Kontakt" path="#contact"/>
-        </div>
+    <div id="hero-element" class="max-w-screen-md p-8 m-auto rounded-sm bg-text_on_primary bg-opacity-75">
+      <h1 class="text-4xl font-bold text-light_white mb-8">Mit Liebe handgemacht</h1>
+      <p class="text-2xl text-light_white mb-8">
+        Aus einer Vielzahl an Materialien gestalte ich individuelle Dekorationen, Geschenke und Mitbringsel.
+        In meinem Sortiment findest du einzelne Artikel, Komplettsets sowie personalisierte Designs nach deinen
+        Wünschen.
+      </p>
+      <div class="flex flex-col sm:flex-row flex-wrap gap-8 sm:gap-32">
+        <ButtonAnchorPrimary title="Zum WhatsApp Shop" path="https://wa.me/c/491637917232"/>
+        <ButtonAnchorSecondary title="Kontakt" path="#contact"/>
       </div>
+    </div>
   </section>
 </template>
 
@@ -24,6 +24,7 @@ import ButtonAnchorPrimary from "~/components/elements/ButtonAnchorPrimary.vue";
 #hero {
   min-height: calc(100svh - 6rem);
 }
+
 .hero-background-img {
   background-image: url('public/melanie/melanie_bg.png');
 }
