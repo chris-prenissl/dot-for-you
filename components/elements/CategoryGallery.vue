e1af5f524d709b3d5fda36d9237b92add8e0b643
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-09 18:32:56 +0200

    Add navigation and cover-flow effect

--- components/elements/CategoryGallery.vue
+++ components/elements/CategoryGallery.vue
@@ -9,19 +9,17 @@ defineProps<{
 
 <template>
   <Swiper class="w-full sm:w-1/2 m-auto drop-shadow-lg"
-          :modules="[SwiperAutoplay, SwiperEffectFade]"
-          :effect="'fade'"
+          :modules="[SwiperAutoplay, SwiperNavigation, SwiperPagination, SwiperEffectCoverflow]"
+          :effect="'coverflow'"
+          :autoplay="true"
           :delay="200"
-          :speed="2000"
-          :fade-effect="{
-            crossFade: true
-          }"
           :centered-slides="true"
           :centered-slides-bounds="true"
           :slides-per-view="1"
           :loop="true"
-          :autoplay="true"
-          :allow-touch-move="false"
+          :navigation="{
+            enabled: true
+          }"
   >
     <SwiperSlide
         v-for="(image, index) in category.images"
@@ -31,3 +29,40 @@ defineProps<{
   </Swiper>
 </template>
 
+<style>
+.swiper-pagination-bullet {
+  background: grey !important;
+}
+
+.swiper-pagination-bullet-active {
+  width: 30px !important;
+  border-radius: 10px;
+}
+
+.swiper-button-prev {
+  color: grey;
+}
+
+.swiper-button-prev::after {
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+}
+
+.swiper-button-next::after {
+  @media (max-width: 768px) {
+    font-size: 1rem;
+  }
+}
+
+.max-h-1080px {
+  max-height: 1080px !important;
+}
+
+.swiper-button-next {
+  color: grey;
+}
+.gallery-radial-gradiant {
+  background: radial-gradient(circle,#f7d3df 10%, #f9f9f9);
+}
+</style>
