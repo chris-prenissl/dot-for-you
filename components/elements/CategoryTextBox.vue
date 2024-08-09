fc5df734685bfa13037b42a9345f2bf4249d4a79
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-09 03:18:48 +0200

    Center description

--- components/elements/CategoryTextBox.vue
+++ components/elements/CategoryTextBox.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="h-40svh sm:h-auto sm:w-1/2 bg-light_white text-text_on_primary p-8 border-2">
+  <div class="sm:w-1/2 bg-light_white content-center text-text_on_primary p-8 border-2">
     <p>{{ description }}</p>
   </div>
 </template>
