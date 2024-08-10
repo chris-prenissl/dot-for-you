6c3c01528506e6f5a4558cd809c3a2d6b0aa75b5
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-10 14:08:25 +0200

    Update Cookie code

--- components/elements/CookieDialog.vue
+++ components/elements/CookieDialog.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
-const cookieAccept = useCookie('accept', {
+const acceptCookieKey = 'accept';
+const acceptCookieValue = 'yes';
+
+const cookieAccept = useCookie(acceptCookieKey, {
   //expires in one month
   expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
 });
 
 function onAcceptCookies(): void {
-  cookieAccept.value = 'true';
+  cookieAccept.value = acceptCookieValue;
 }
 </script>
 
 <template>
-  <div v-if="!cookieAccept" class="fixed bottom-0 w-full z-50 flex flex-wrap gap-2 justify-center items-center bg-light_white p-2">
+  <div v-if="cookieAccept !== acceptCookieValue" class="fixed bottom-0 w-full z-50 flex flex-wrap gap-2 justify-center items-center bg-light_white p-2">
     <p>Wir speichern lediglich ein Cookie für die Kenntnisnahme dieses Dialogs. Weitere Cookies oder Daten werden nicht erfasst.</p>
     <button @click="onAcceptCookies" class="p-4 bg-primary border-pink-400 border rounded-sm text-center text-l hover:bg-light_white hover:text-logo_text hover:drop-shadow-lg">Zustimmung</button>
   </div>
