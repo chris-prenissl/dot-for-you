37d4ee1d1579069371ed4a3d48bc0c165b33d97d
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-11 13:18:46 +0200

    Better layout

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,9 +1,9 @@
 <template>
-  <section id="about" class="about-linear-gradient backdrop-blur flex flex-wrap items-center justify-center z-7">
-    <NuxtImg data-aos="fade-right" class="w-full h-auto sm:w-1/2 md:max-w-screen-md border-8 content-center object-cover shadow-2xl" src="/melanie/melanie.jpg" format="webp"  sizes="sm:40svw lg:100svw" loading="lazy" placeholder alt="Melanie"/>
+  <section id="about" class="about-linear-gradient backdrop-blur flex flex-wrap gap-2 items-center justify-center z-7">
+    <NuxtImg data-aos="fade-right" class="h-50svh sm:w-1/2 md:max-w-screen-md border-8 content-center object-cover shadow-2xl" src="/melanie/melanie.jpg" format="webp"  sizes="sm:40svw lg:100svw" loading="lazy" placeholder alt="Melanie"/>
     <div data-aos="fade-left" class="sm:max-w-screen-lg p-2 sm:p-4 bg-light_white bg-opacity-50 rounded-l backdrop-blur-2xl shadow-2xl border-2 border-primary">
-      <h2 class="pb-2 text-4xl font-bold">About Me</h2>
-      <p class="text-sm md:text-lg md:max-w-screen-l text-justify text-text_on_primary border-dotted">
+      <h2 class="p-2 text-3xl font-bold">About Me</h2>
+      <p class="text-sm md:text-lg md:max-w-screen-l p-2 text-justify text-text_on_primary border-dotted">
         Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
         drei Katzen in Burglengenfeld. Hauptberuflich arbeite ich als allgemeinbildende Lehrerin an einer
         sonderpädagoischen Berufsschule. Meinen Ausgleich finde ich in meinem kreativen Hobby, welches sich zu einem
