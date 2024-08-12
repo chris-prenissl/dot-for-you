8cd385d9c67c6d7d5595811810c49db7c7097e84
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-12 17:25:54 +0200

    Add favicon and title with description

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,4 +12,18 @@ export default defineNuxtConfig({
         viewer: true
     },
     compatibilityDate: '2024-08-10',
+    app: {
+        head: {
+            title: 'DotForYou',
+            meta: [
+                { charset: 'utf-8' },
+                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+                { name: 'description', content: 'Mit Liebe. handgemacht.' }
+            ],
+            link: [
+                { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+            ]
+        },
+
+    }
 });
