17606e2b7c1fb01e0707cbf22b83f8478ae0d808
Christoh Prenissl <christophprenissl@gmail.com>
2024-08-29 17:25:54 +0200

    Add social media preview related meta tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,11 +14,17 @@ export default defineNuxtConfig({
     compatibilityDate: '2024-08-10',
     app: {
         head: {
-            title: 'DotForYou',
+            title: 'DotForYou - Handmade Kunst für Deko und Geschenke',
             meta: [
                 { charset: 'utf-8' },
                 { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                { name: 'description', content: 'Mit Liebe. handgemacht.' }
+                { name: 'description', content: 'Willkommen bei DotForYou - bei mir findest du kreative und handgemachte Schätze zu vielen Anlässen.' },
+                { property: 'og:title', content: 'DotForYou - Handmade' },
+                { property: 'og:url', content: 'https://dotforyou.de/' },
+                { property: 'og:description', content: 'Kreative und handgemachte Schätze zu vielen Anlässen' },
+                { property: 'og:image', content: 'https://dotforyou.de/webpage.jpeg' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:locale', content: 'de_DE' },
             ],
             link: [
                 { rel: 'icon', type: 'image/png', href: '/icon.png' },
