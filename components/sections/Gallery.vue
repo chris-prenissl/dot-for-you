7bf0811e0ec4b7d436ab17ff4a4ac8e6c0d7e037
Christoh Prenissl <christophprenissl@gmail.com>
2024-11-21 14:23:09 +0100

    Update gallery images

--- components/sections/Gallery.vue
+++ components/sections/Gallery.vue
@@ -10,7 +10,7 @@ function shuffleArray<T>(array: T[]) {
 }
 
 const images: Array<GalleryImage> = [];
-for (let i = 1; i <= 30; i++) {
+for (let i = 1; i <= 32; i++) {
   images.push({
     title: "Galeriebild " + i,
     url: "/gallery_images/gallery_img - " + i + ".png"
