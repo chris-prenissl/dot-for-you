394bb0be6c1fa2ea4902b0eab8fb0b9882331c26
Christoh Prenissl <christophprenissl@gmail.com>
2025-02-23 00:20:03 +0100

    Update layout

--- components/elements/CategoryItem.vue
+++ components/elements/CategoryItem.vue
@@ -14,7 +14,7 @@ defineProps<{
     <div class="md:max-w-screen-xl flex flex-col md:flex-row m-auto">
       <CategoryTextBox class="order-last md:-order-1" v-if="gridOrder === 0" :title="category.title" :background-url="category.coverImgUrl"
                        :description="category.description" data-aos="fade-right"/>
-      <ArtImage class="w-full h-40svh md:w-1/2 md:h-50svh" :src-path="category.coverImgUrl" :title="category.title" img-sizes="sm:40svw lg:100svw" :data-aos="gridOrder === 0 ? 'fade-left' : 'fade-right'"/>
+      <ArtImage class=" md:w-1/2 md:h-50svh" :src-path="category.coverImgUrl" :title="category.title" img-sizes="sm:40svw lg:100svw" :data-aos="gridOrder === 0 ? 'fade-left' : 'fade-right'"/>
       <CategoryTextBox  v-if="gridOrder === 1" :title="category.title" :description="category.description"
                        data-aos="fade-left" :background-url="category.coverImgUrl"/>
     </div>
--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,13 +1,15 @@
 <template>
-  <section id="about" class="about-linear-gradient backdrop-blur flex flex-wrap gap-2 items-center justify-center z-6">
-    <NuxtImg data-aos="fade-right" class="h-50svh md:w-1/2 md:max-w-screen-md border-8 content-center object-cover shadow-2xl" src="/melanie/melanie.jpg" format="webp"  sizes="sm:40svw lg:100svw" loading="lazy" placeholder alt="Melanie"/>
-    <div data-aos="fade-left" class="md:max-w-screen-lg p-2 md:p-4 bg-light_white bg-opacity-50 rounded-l backdrop-blur-2xl shadow-2xl border-2 border-primary">
-      <h2 class="p-2 text-3xl font-bold">About Me</h2>
-      <p class="text-sm md:text-lg md:max-w-screen-l p-2 text-justify text-text_on_primary border-dotted">
-        Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
-        drei Katzen in Burglengenfeld. Hauptberuflich arbeite ich als allgemeinbildende Lehrerin an einer
-        sonderpädagoischen Berufsschule. Meinen Ausgleich finde ich in meinem kreativen Hobby.
-      </p>
+  <section id="about" class="about-linear-gradient backdrop-blur content-center z-6">
+    <div class="mx-auto flex flex-wrap gap-2 justify-center items-stretch">
+      <NuxtImg data-aos="fade-right" class="w-full sm:w-2/5 md:max-w-screen-sm border-8 content-center object-cover shadow-2xl" src="/melanie/melanie.jpg" format="webp"  sizes="sm:100svw lg:100svw" loading="lazy" placeholder alt="Melanie"/>
+      <div data-aos="fade-left" class="sm:w-2/5 md:max-w-screen-sm p-2 md:p-4 content-center bg-light_white bg-opacity-50 rounded-l backdrop-blur-2xl shadow-2xl border-2 border-primary">
+        <h2 class="p-2 text-3xl sm:text-xl font-bold">About Me</h2>
+        <p class="md:max-w-screen-l text-m  p-2 text-justify text-text_on_primary border-dotted">
+          Mein Name ist Melanie Zimmer und ich wohne zusammen mit meinem Mann und unseren
+          drei Katzen in Burglengenfeld. Hauptberuflich arbeite ich als allgemeinbildende Lehrerin an einer
+          sonderpädagoischen Berufsschule. Meinen Ausgleich finde ich in meinem kreativen Hobby.
+        </p>
+      </div>
     </div>
   </section>
 </template>
