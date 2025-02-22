3ad0d555afb9dcb0e05e0c5a08fb9d8a3ddebf17
Christoh Prenissl <christophprenissl@gmail.com>
2025-02-22 17:27:48 +0100

    Updated Announcement.ts styling and structure

--- components/sections/Announcements.vue
+++ components/sections/Announcements.vue
@@ -1,35 +1,25 @@
 <script setup lang="ts">
-const image = useImage();
-const backgroundImage = computed(() => {
-  const imgUrl = image('/ankuendingungen_bg.png', {format: 'webp', sizes: 'sm:30svw lg:10svw'})
-  return {
-    backgroundImage: `url('${imgUrl}')`
-  }
-})
+import type {Announcement} from "~/model/Announcement";
+
+const announcements: Announcement[] = [
+  { title: 'Frühlingsausstellung', date: '14.03.2025 - 9 bis 17 Uhr', location: 'Haus des Guten Hirten - Klosterladen, Schwandorf' },
+  { title: 'Frühlingsausstellung', date: '15.03.2025 - 10 bis 16 Uhr', location: 'Haus des Guten Hirten - Klosterladen, Schwandorf' },
+  { title: 'Osterausstellung', date: '06.04.2025 - 10 bis 17 Uhr', location: 'Jahnhalle, Regenstauf' },
+  { title: 'Frühlingsmarkt Kallmünz', date: '03.05.2025', location: 'Kallmünz' }
+];
 </script>
 
 <template>
 <section id="announcements" class="min-h-min z-9">
-  <div class="announcement-box m-auto text-xs text-center content-center bg-center bg-contain bg-no-repeat"
-       :style="backgroundImage">
-    <div class="m-auto p-10 -rotate-3">
-      <h2 class="pb-2 text-lg">Ankündigungen</h2>
-      <h3 class="underline">Markttermine</h3>
-      <ul>
-        <li>14.03.2025 9 bis 17 Uhr - Frühlingsausstellung im Haus des Guten Hirten, 92421 Schwandorf</li>
-        <li>15.03.2025 10 bis 16 Uhr - Frühlingsausstellung im Haus des Guten Hirten, 92421 Schwandorf</li>
-        <li>06.04.2025 - Marktstand auf der Osterausstellung in der Jahnhalle,<br> 93128 Regenstauf</li>
-        <li>03.05.2025 - Frühlingsmarkt Kallmünz, 93183 Kallmünz</li>
-      </ul>
+  <div class="max-w-2xl mx-auto p-6 m-9 bg-primary shadow-lg rounded-2xl">
+    <h2 class="text-2xl font-semibold text-center text-text_on_primary pb-4">🔔 Markttermine</h2>
+    <div class="space-y-4">
+      <div v-for="announcement in announcements" :key="announcement.date + announcement.title" class="p-4 bg-light_white rounded-lg shadow-sm">
+        <h3 class="text-xl font-medium">{{ announcement.title }}</h3>
+        <p class="text-sm">{{ announcement.date }}</p>
+        <p class="text-text_default">{{ announcement.location }}</p>
+      </div>
     </div>
   </div>
 </section>
 </template>
-
-<style scoped>
-.announcement-box {
-  max-width: 36rem;
-  height: 36rem;
-  background-size: 105%;
-}
-</style>
---
+++ model/Announcement.ts
@@ -0,0 +1,5 @@
+export type Announcement = {
+    title: string,
+    date: string,
+    location: string,
+}
