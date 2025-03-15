1915e1f37a78627f6597840905daa38b56940144
Christoh Prenissl <christophprenissl@gmail.com>
2025-03-15 15:29:59 +0100

    Add Sicherheitshinweise

---
+++ pages/sicherheitshinweise.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="flex justify-center mt-6">
+    <div class="bg-white rounded shadow p-6 w-full max-w-xl text-left">
+      <h1 class="text-4xl font-bold">Gebrauchs- und Sicherheitshinweise</h1>
+
+      <div class="mt-6">
+        <h2 class="text-2xl font-semibold mt-12">Allgemeine Hinweise</h2>
+        <p class="text-lg mt-6">
+          Alle Artikel sind ausschließlich zu dekorativen Zwecken gedacht und dürfen nicht zweckentfremdet werden. Alle
+          angebotenen Artikel sind kein Spielzeug. Kinder und Haustiere sollen sich nicht unbeaufsichtigt in der Nähe
+          der Dekoartikel aufhalten, um Verletzungen oder Verschlucken zu vermeiden. Der Hersteller (dot for you)
+          haftet nicht für Schäden, der durch unsachgemäßen Gebrauch entsteht. Alle Artikel und Verpackungsmaterialien
+          müssen nach den örtlichen Entsorgungsvorschriften entsorgt werden.
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Kerzen</h2>
+        <p class="text-lg">
+          Lassen Sie brennende Kerzen niemals unbeaufsichtigt. Stellen Sie die Kerze immer auf eine hitzebeständige,
+          stabile und nicht entflammbare Unterlage. Halten Sie die Kerze von brennenden Materialien, Vorhängen und
+          anderen Gegenständen sowie Zugluft fern. Kerzen sollen nicht vollständig abbrennen und rechtzeitig gelöscht
+          werden. Die verwendeten Kerzenfolien (Schriften und Bilder auf den Kerzen) sind speziell zum Abbrennen
+          hergestellt und können somit zusammen mit der Kerze abbrennen. Nach längerem Aufbewahren können die
+          Kerzenfolien ihre Stabilität verändern. Entfernen Sie Trockenblumen, Folien und andere dekorative Gegenstände
+          von den Kerzen, bevor Sie sie anzünden. Kleine Kerzenreste können im Hausmüll entsorgt werden.
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Holzprodukte</h2>
+        <p class="text-lg">
+          An Holzsplittern besteht die Gefahr sich zu verletzten. Halten Sie das Produkt von offenen Flammen und
+          Wärmequellen fern, da es entflammbar ist. Das Holz sollte vor Witterung und Wasser ferngehalten werden.
+          Das Holz kann als Heizmaterial verwendet werden. Holz entsorgen sie nach den örtlichen
+          Entsorgungsvorschriften (Wertstoffhof).
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Trockenblumen</h2>
+        <p class="text-lg">
+          Die Trockenblumen sind leicht entflammbar und sollen vor offenen Flammen und Wärmequellen ferngehalten werden.
+          Um Schimmelbildung zu vermeiden sollen die Trockenblumen nicht in feuchter oder nasser Umgebung lagern. Sie
+          sind nicht zum Verzehr geeignet. Trockenblumen können ggf. allergische Reaktionen hervorrufen. Entsorgung
+          kann über den Biomüll oder den Hausmüll erfolgen.
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Reliefgießmasse</h2>
+        <p class="text-lg">
+          Die fertigen Produkte sind bruchempfindlich und daher mit Vorsicht zu behandeln. An Bruchstellen können
+          scharfe Kanten sowie Splitter zu Verletzungen führen. Atmen Sie möglichen Staub nicht direkt ein. Gegossene
+          Artikel sind nicht für den Kontakt mit Wasser, echten Pflanzen oder Lebensmitteln geeignet. Bei sehr hohen
+          Minusgraden können die Artikel ggf. brechen. Zur Säuberung können Sie die Produkte abstauben oder nebelfeucht
+          abwischen. Kerzen – und Teelichthalter sowie alle anderen Artikel sind nicht feuerbeständig. Löschen Sie die
+          Kerze rechtzeitig. Kleine Lufteinschlüsse oder Unebenheiten sind gießtypisch und kein Reklamationsgrund.
+          Kleinere Mengen können über den Hausmüll entsorgt werden. Größere Mengen entsorgen Sie auf Ihrem örtlichen
+          Wertstoffhof.
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Dekorative Kleinteile und Verzierungen</h2>
+        <p class="text-lg">
+          Kleinteile und Verzierungen jeglicher Art (Bsp. Glasröhrchen, Dekosteine, Bast, Engelshaar, Schnüre, Anhänger,
+          Draht u.ä.) sind nicht geeignet für Kleinkinder und Haustiere. Erwachsene tragen hier Verantwortung für Kinder
+          und Haustiere. Beachten Sie, dass man Kleinteile und Verzierungen beispielsweise verschlucken kann, ebenso
+          kann man sich schneiden oder anderweitig verletzen. Teilweise sind die Kleinteile und Verzierungen leicht
+          entflammbar und sollten daher von Feuer ferngehalten werden. Entsorgen Sie die Kleinteile nach Vorschrift den
+          örtlichen Entsorgungsvorschriften. Lichterketten dürfen nur mit empfohlenen Batterietypen verwendet werden.
+          Setzen Sie Batterien keiner Hitze oder Feuchtigkeit aus. Entfernen Sie Batterien, wenn das Produkt längere
+          Zeit nicht verwendet wird. Batterien können auslaufen und ätzende Flüssigkeit freisetzen. Bei Kontakt sofort
+          gründlich mit Wasser spülen und einen Arzt aufsuchen. Batterien kann man kostenlos an kommunalen Sammelstellen
+          oder im Handel zurückgeben.
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Dotpainting/ Handbemalte Keramik</h2>
+        <p class="text-lg">
+          Die verwendeten Farben sind spezielle Glas- und Porzellanmalfarben der Marke Kreul, welche nach dem Einbrennen
+          laut Hersteller wasserfest, spülmaschinenfest und speichelecht sind. Die Farbe auf den bemalten Tassen und
+          Gläsern wird von mir eingebrannt. Die Farben auf Seifenspender und Flaschenlichter sind luftgetrocknet und
+          somit wasserfest. Sie können diese Artikel per Hand feucht abwischen. Jegliche Art von Keramik kann brechen.
+          An scharfen Kanten besteht Verletzungsgefahr. Glas und Keramik sollen im Glascontainer entsorgt werden.
+        </p>
+
+        <h2 class="text-2xl font-semibold mt-12">Kontaktieren Sie mich gerne, sollten Sie noch Fragen haben.</h2>
+
+        <h3 class="text-lg mt-8">Kontakt</h3>
+        <p class="text-lg">
+          Melanie Zimmer<br />
+          Augustenstr. 20<br />
+          93133 Burglengenfeld<br />
+          Telefon: 0163 / 7917232<br />
+          Website: <a href="https://dotforyou.de" class="text-blue-500">dotforyou.de</a>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
