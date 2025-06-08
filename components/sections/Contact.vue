19206b8e93e27b2c8e2ccdd792265724d3db1cfc
Christoh Prenissl <christophprenissl@gmail.com>
2025-06-08 20:35:51 +0200

    Remove Facebook social link and align social items to center on bigger screens

--- components/sections/Contact.vue
+++ components/sections/Contact.vue
@@ -2,7 +2,7 @@
   <section id="contact" class="m-auto flex flex-col bg-primary text-light_text z-5">
     <h1 class="section-header text-text_on_primary">Kontakt</h1>
     <div class="m-auto flex flex-col justify-evenly py-2 md:py-8">
-      <div class="flex flex-col items-start flex-wrap md:flex-row md:items-center px-16 py-2 md:py-16 gap-1 md:gap-6">
+      <div class="flex flex-col items-start flex-wrap md:flex-row md:items-center px-16 py-2 md:py-16 gap-1 md:gap-6 md:justify-center">
         <SocialItem href="https://wa.me/c/491637917232" text="WhatsApp Shop">
           <WhatsAppBusiness/>
         </SocialItem>
@@ -12,12 +12,9 @@
         <SocialItem href="https://www.instagram.com/dot_for_you_" text="Instagram">
           <Instagram/>
         </SocialItem>
-        <SocialItem href="https://m.facebook.com/61560145346862/" text="Facebook">
-          <Facebook/>
-        </SocialItem>
       </div>
       <div
-          class="flex flex-col flex-wrap lg:flex-row gap-1 justify-center items-center m-auto text-center">
+          class="flex flex-col flex-wrap lg:flex-row gap-1 lg:justify-center items-start lg:items-center m-auto text-center">
         <AddressItem href="tel:+491637917232" text="+49 163 791 72 32">
           <Phone/>
         </AddressItem>
@@ -40,7 +37,6 @@ import Location from "~/components/svgs/Location.vue";
 import EMail from "~/components/svgs/EMail.vue";
 import Phone from "~/components/svgs/Phone.vue";
 import SocialItem from "~/components/elements/SocialItem.vue";
-import Facebook from "~/components/svgs/Facebook.vue";
 import Instagram from "~/components/svgs/Instagram.vue";
 import WhatsApp from "~/components/svgs/WhatsApp.vue";
 import WhatsAppBusiness from "~/components/svgs/WhatsAppBusiness.vue";
